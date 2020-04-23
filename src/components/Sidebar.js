e5803a6f878633bffd31526209dea6253e2267a7
therjmurray <therjmurray@gmail.com>
2020-04-23 17:32:10 -0400

    change ugly colors

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,8 +8,8 @@ const Sidebar = () => {
 
     let parsedLocations = Object.keys(locations)
 
-    return <div style={{width: 300, background: '#E7DE7D'}}>
-        {parsedLocations.map((e, i) => <div onClick={() => setView(e)} key={i} style={{width: '100%', height: 40, background: '#D7ED7E', borderBottom: '1px solid green'}}>
+    return <div style={{width: 300, background: '#D7D7D7', borderRight: '1px solid black'}}>
+        {parsedLocations.map((e, i) => <div onClick={() => setView(e)} key={i} style={{width: '100%', height: 40, background: '#F8F8F8', borderBottom: '1px solid green'}}>
             <p style={{margin: 0, padding: 10, userSelect: 'none', cursor: 'pointer'}}>{e}</p>
         </div> )}
     </div>
