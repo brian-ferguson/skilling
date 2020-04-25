f284f826f910956cf64bb4a422098cb7661a33e1
therjmurray <therjmurray@gmail.com>
2020-04-25 12:52:21 -0400

    larger activity icons

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -13,13 +13,13 @@ const Activity = (props) => {
 		addItem(e.target.id);
 	}
 
-	return <div onClick={handleActivity} style={{border: '1px solid black', width: 40, margin: '5px 0 0 5px', height: 40, cursor: 'pointer'}}>
+	return <div onClick={handleActivity} style={{border: '1px solid black', width: 60, margin: '5px 0 0 5px', height: 60, cursor: 'pointer'}}>
 		{/* Image */}
 		<img
 			id={props.id}
 			src={process.env.PUBLIC_URL + props.source}
 			alt=""
-			style={{ width: 24, height: 24, paddingBottom: 5, marginLeft: 10 }}
+			style={{ width: 40, height: 40, paddingBottom: 5, marginLeft: 10 }}
 		/>
 	</div>
 };
