4bf7a03337210f5b253c976535be3513c440a62e
therjmurray <therjmurray@gmail.com>
2020-04-25 12:28:44 -0400

    rename handleRefresh to handleActivity

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -7,13 +7,13 @@ const Activity = (props) => {
 	const { addItem } = playerContext;
 	// const { inventory, addItem } = playerContext;
 
-	const handleRefresh = e => {
+	const handleActivity = e => {
 
 		//var item_drop = props.drop;
 		addItem(e.target.id);
 	}
 
-	return <div onClick={handleRefresh} style={{border: '1px solid black', width: 40, margin: '5px 0 0 5px', height: 40, cursor: 'pointer'}}>
+	return <div onClick={handleActivity} style={{border: '1px solid black', width: 40, margin: '5px 0 0 5px', height: 40, cursor: 'pointer'}}>
 		{/* Image */}
 		<img
 			id={props.id}
