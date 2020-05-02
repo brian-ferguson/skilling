4958790695f8458ade1d5532f02294a8aeae1615
Brian Ferguson <brianghferguson@gmail.com>
2020-05-02 18:27:34 -0400

    Reworking

--- src/context/PlayerContext.js
+++ src/context/PlayerContext.js
@@ -3,35 +3,91 @@ import PropTypes from "prop-types";
 import locations_json from '../json/locations.json'
 import activities_json from '../json/activities.json'
 import items_json from '../json/items.json'
+import actions_json from '../json/actions.json'
 
-//context
 export const Context = createContext({});
 
-//provider
 export const Provider = props => {
-	const [view, setView] = useState('Calm Beach')
-	const [inventory, setInventory] = useState([]);
-	const [stats, setStats] = useState([])
-	const [locationActivities, setLocationActivities] = useState([]);
+
+	const [locations, setLocations] = useState([]);
+	const [locationActivities, setLocationActivities] = useState([])
+	const [locationActions, setLocationActions] = useState([])
+	const [view, setView] = useState("cb881ee5-06fa-4272-9fe7-2937ab4e002a")
 	const [work, setWork] = useState(false)
 	const [messages, setMessages] = useState([])
+	const [inventory, setInventory] = useState([])
+	const [stats, setStats] = useState([])
 
 	useEffect(() => {
-		let currentLocations = Object.keys(locations_json[view])
-		let currentLocationActivities = [];
 
-		//get all the activities of the current location
-		for (let i = 0; i < currentLocations.length; i++) {
-			let currentActivity = activities_json[currentLocations[i]]
-			currentLocationActivities.push(currentActivity)
+		//initialize an array to hold the available location objects
+		let currentLocations = [];
+		//initialize an array to hold the current view location activities
+		let currentLocationActivities = [];
+		//initialize an array to hold the current location activity actions
+		let currentLocationActions = [];
+
+		//iterate through the game locations
+		for(let i=0; i<locations_json["locations"].length; i++){
+			//add the location object at the current index to currentLocations
+			currentLocations.push(locations_json["locations"][i]);
+			//if the location object at the current index id is equal to the view id
+			if(locations_json["locations"][i].id === view){
+				//iterate through the activities of the current view location
+				for(let j=0; j<locations_json["locations"][i].activities.length; j++){
+					//iterate through the game activity objects
+					for(let k=0; k<activities_json["activities"].length; k++){
+						//instantiate an array to hold the actions of each activity
+						let currentActivityActions = [];
+						//if the id of the activity of the current view location is equal to the id the game object activity
+						if(activities_json["activities"][k].id === locations_json["locations"][i].activities[j]){
+							//add the activity to the list of location activities
+							currentLocationActivities.push(activities_json["activities"][k]);
+							//iterate through the the activity actions
+							for(let l=0; l<activities_json["activities"][k].actions.length; l++){
+								//iterate through the actions
+								for(let m=0; m<actions_json["actions"].length; m++){
+									//if the activity action id and the action id are equal
+									if(actions_json["actions"][m].id === activities_json["activities"][k].actions[l]){
+										//add the action object to current activity actions array
+										currentActivityActions.push(actions_json["actions"][m]);
+									}
+								}
+							}
+						}
+
+						//add the current activity actions to currentLocationActions
+						if(currentActivityActions.length !== 0){
+							currentLocationActions.push(currentActivityActions);
+						}
+
+					}
+				}
+			}
 		}
 
-		if(currentLocationActivities){
+		//if the list of locations is not 0
+		if(currentLocations.length !== 0){
+			//update the locations state
+			setLocations(currentLocations);
+		}
+
+		//if the list of current location activities is not 0
+		if(currentLocationActivities.length !== 0){
+			//update the current location activities state
 			setLocationActivities(currentLocationActivities);
 		}
+
+		//if the list of current location actions is not 0
+		if(currentLocationActions.length !== 0){
+			console.log("did it work?", currentLocationActions);
+			setLocationActions(currentLocationActions);
+		}
+
 	}, [view])
 
-	const levelFormula = exp => {
+	//takes an amount of experience and returns the equivalent level amount
+	const levelFormula = (exp) => {
 		return Math.floor(Math.sqrt(exp) * 1.2);
 	};
 
@@ -42,12 +98,9 @@ export const Provider = props => {
 
 	//takes an item id and returns the quantity of that item in inventory
 	const checkInventory = (item) => {
-		//get the index of the item in inventory
 		let inventoryIndex = checkInventoryIndex(item);
-		//if the index does not exist, return 0
 		if(inventoryIndex === -1){
 			return 0
-		//if the index does exist, return the quantity
 		}else{
 			return inventory[inventoryIndex].quantity;
 		}
@@ -75,12 +128,12 @@ export const Provider = props => {
 		return true
 	}
 
-	//takes the id of an item and returns the item object
-	const getItemObject = (id) => {
-		for (var key in items_json) {
-			if (items_json.hasOwnProperty(key)) {
-				if(items_json[key].id === id){
-					return items_json[key]
+	//takes and id and json object and returns the corresponding object
+	const getObject = (id, objectList) => {
+		for (var key in objectList) {
+			if (objectList.hasOwnProperty(key)) {
+				if(objectList[key].id === id){
+					return objectList[key]
 				}
 			}
 		}
@@ -88,6 +141,10 @@ export const Provider = props => {
 
 	//takes the id of an activity (integer)
 	const doActivity = (activity) => {
+
+		console.log("activity id: ", activity);
+
+		/*
 
 		//methods and functions
 		//selectDrop: selects a item to drop from weighted probabilities
@@ -116,8 +173,6 @@ export const Provider = props => {
 					return returnItem
 				}
 			}
-
-
 		}
 
 		//updateInventory
@@ -144,7 +199,7 @@ export const Provider = props => {
 				//if the item to be collected does not exist in inventory
 				if(checkInventoryIndex(add[j].id) === -1){
 					//get the item object
-					let addObject = getItemObject(add[j].id);
+					let addObject = getObject(add[j].id, items_json);
 					addObject.quantity = add[j].quantity;
 					//add the item to the inventory
 					new_inventory.push(addObject);
@@ -161,8 +216,8 @@ export const Provider = props => {
 
 			for(let i = 0; i < drop.length; i++){
 				//get the item object by id
-				let dropObject = getItemObject(drop[i].id)
-				
+				let dropObject = getObject(drop[i].id, items_json)
+
 				if (stats.filter(e => e.name === dropObject.experienceType).length > 0) {
 					let current_experience = stats[stats.map(i => i.name).indexOf(dropObject.experienceType)].experience
 					let stat_index = stats.map(i => i.name).indexOf(dropObject.experienceType)
@@ -190,7 +245,7 @@ export const Provider = props => {
 
 			let currentDrop = selectDrop(activities_json[activity].drop)
 			updateStats(currentDrop)
-			setMessages([...messages, `You ${activityType.toLowerCase()} a ${currentDrop.name} and gain ${currentDrop.experience} ${currentDrop.experienceType} experience.`])
+			//setMessages([...messages, `You ${activityType.toLowerCase()} a ${currentDrop.name} and gain ${currentDrop.experience} ${currentDrop.experienceType} experience.`])
 			updateInventory(currentDrop)
 
 		//if the activity type is Refine
@@ -201,21 +256,16 @@ export const Provider = props => {
 			let check = checkInventoryRequirements(itemRequirements)
 
 			if(check){
-				let string = ''
-				for (let i = 0; i < itemRequirements.length; i++) {
-					string = string.concat(itemRequirements[i].quantity + ' ')
-					string = string.concat(items_json[itemRequirements[i].id].name)
-					if (i + 2 >= itemRequirements.length) {
-						string = string.concat(' and ')
-					} else {
-						string = string.concat(', ')
-					}
-				}
-				setMessages([...messages, `You ${activityType.toLowerCase()} ${string} gained ${itemOutputs.experience} ${itemOutputs.experienceType} experience.`])
+
+
+				//setMessages([...messages, `You ${activityType.toLowerCase()} ${string} gained ${itemOutputs.experience} ${itemOutputs.experienceType} experience.`])
 				updateStats(itemOutputs)
 				updateInventory(itemOutputs, itemRequirements)
 			}
 		}
+
+
+		*/
 	};
 
 	const playerContext = {
@@ -231,7 +281,12 @@ export const Provider = props => {
 		stats,
 		locationActivities,
 		setLocationActivities,
-		checkInventoryRequirements
+		checkInventoryRequirements,
+		locations,
+		setLocations,
+		locationActions,
+		setLocationActions
+
 	};
 
 	return <Context.Provider value={playerContext}>{props.children}</Context.Provider>
@@ -245,13 +300,11 @@ export const { Consumer } = Context;
 Provider.propTypes = {
   inventory: PropTypes.array,
   items: PropTypes.array,
-	activities: PropTypes.array,
 	locations: PropTypes.array,
 };
 
 Provider.defaultProps = {
   inventory: [],
   items: [],
-	activities: [],
 	locations: [],
 };
