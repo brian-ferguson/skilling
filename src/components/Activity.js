29aa1d5662b6605cdbcc68ab0d19a85cd210e82a
therjmurray <therjmurray@gmail.com>
2020-05-04 20:10:11 -0400

    checkInventoryRequirements --wip

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,11 +1,14 @@
-import  React, {useContext} from "react";
+import  React, { useState, useEffect, useContext } from "react";
 import { PlayerContext } from "../context";
+import ProgressBar from '../tools/ProgressBar'
+import actions_json from '../json/activities.json'
+import items_json from '../json/items.json'
 
 const container_styles = {
 	border: '1px solid #7D7D7D',
 	borderRadius: 4,
 	width: 200,
-	height: 120,
+	height: 150,
 	margin: '10px 0 0 10px',
 	cursor: 'pointer',
 	userSelect: 'none',
@@ -28,12 +31,46 @@ const button_styles = {
 }
 
 const Activity = (props) => {
-
 	const playerContext = useContext(PlayerContext);
-	const {doActivity} = playerContext;
+	const { doActivity, work, setWork, checkInventoryRequirements } = playerContext;
+	const [time, setTime] = useState(0)
+	const [available, setAvailable] = useState(true)
+	const [action, setAction] = useState("")
 
-	const handleActivity = e => {
-		doActivity(props.id, e.target.id)
+	const divisor = 2
+
+	useEffect(() => {
+        if(!available){
+            const interval = setInterval(() => {
+				if(checkInventoryRequirements()) {
+					if(time < 100) {
+						setTime(time + divisor)
+						doActivity(props.id, action)
+					} else {
+						setWork(false)
+						setAvailable(true)
+						setTime(0)
+					}
+				}
+            }, 1250);
+            return () => clearInterval(interval);
+        }
+    }, [time, work, props.id, doActivity, setWork, available, action, checkInventoryRequirements])
+
+	const startActivity = (e) => {
+		setAction(e.target.id)
+        if(!work) {
+			setAvailable(false)
+			setWork(true)
+			setTime(divisor)
+			doActivity(props.id, e.target.id)
+        }
+    }
+
+	const cancelActivity = () => {
+		setAvailable(true)
+		setWork(false)
+		setTime(0)
 	}
 
 	return <div style={container_styles}>
@@ -51,18 +88,19 @@ const Activity = (props) => {
 			<p style={{margin: '25px 0 0 10px', fontSize: 18}}>{props.name}</p>
 		</div>
 
+		{/* <Line percent={time} strokeWidth="10" trailWidth="10" strokeColor={time === 100 ? 'green' : 'brown'} strokeLinecap="square" style={{margin: 5}} /> */}
+		<ProgressBar width={time} />
+
 		{/* Button Container */}
 		<div style={{display: 'flex', width: '100%', justifyContent: 'space-evenly'}}>
 
 			{/* Buttons */}
-			{props.actions !== undefined ?
-
-				props.actions.map((e, i) => <button id={e.id} key={e.id} onClick={handleActivity} style={button_styles}>{e.name}</button>)
-
-				: null
-
+			{!available 
+				? <button id={props.id} onClick={cancelActivity} style={{...button_styles, background: 'red'}}>Stop</button>
+				: props.actions !== undefined 
+					? props.actions.map((e, i) => <button id={e.id} key={e.id} onClick={startActivity} style={button_styles}>{e.name}</button>)
+					: null
 			}
-
 		</div>
 	</div>
 };
