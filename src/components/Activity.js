ba7a3a10c6778f942a499fe4504f7c360b052a93
Brian Ferguson <brianghferguson@gmail.com>
2020-05-09 13:42:42 -0400

    Added cycle time and interval to actions, modified activity to use seconds and added fishing rudimentarily in activity

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -34,27 +34,51 @@ const Activity = (props) => {
 	const playerContext = useContext(PlayerContext);
 	const { doActivity, work, setWork, checkInventoryRequirements, getObject } = playerContext;
 	const [time, setTime] = useState(0)
+	const [actionTime, setActionTime] = useState(0)
 	const [available, setAvailable] = useState(true)
 	const [action, setAction] = useState("")
 
-	const divisor = 2
+	const timeSwitch = (actionType, actionTime, actionInterval) => {
 
-	const timeSwitch = (actionType) => {
+		//increment the time state by 1 second
+		setTime(time + 1);
 
-		let finished = false;
+		if(time === actionTime){
+			if(actionType === "Craft"){
+				doActivity(props.id, action)
+			}
+			
+			setWork(false)
+			setAvailable(true)
+			setTime(0)
+		}else if(time % actionInterval === 0  && time !== 0){
+			//if the action type is of type collected
+			if(actionType === "Collect" || actionType === "Refine"){
+				doActivity(props.id, action)
+			}else if(actionType === "Fishing"){
+				//50% of the time return an item drop
+				let maximum = 1;
+				let mininum = 0;
+				let bite = Math.random() * (maximum - mininum) + mininum;
 
+				console.log("bite: ", bite);
+				//if a bite occurs
+				if(bite === 1){
+					console.log("bite");
+					doActivity(props.id, action);
+				}
+			}
+		}
+		/*
 		switch(true){
-
-			case(time < 100):
-				setTime(time + divisor)
+			case(time < actionTime):
+				setTime(time + 1)
+				console.log("time: ", time);
 				if(actionType === "Collect" || actionType === "Refine"){
 					doActivity(props.id, action)
 				}
-
 				break;
-
 			default:
-
 				setWork(false)
 				setAvailable(true)
 				setTime(0)
@@ -63,30 +87,44 @@ const Activity = (props) => {
 					doActivity(props.id, action)
 				}
 		}
-
+		*/
 	}
 
 	useEffect(() => {
 		//get the item requirements from the action object
 		let actionObject = getObject(action, actions_json["actions"]);
-		let activitiesObject = getObject(props.id, activities_json["activities"])
+		if(actionObject){
+			if(!available){
+				//get the time per cycle of the action object
+				let cycleSeconds = actionObject.cycleSeconds;
+				let totalCyclesAvailable = 1;
+				let cycleTime = cycleSeconds * totalCyclesAvailable;
+				let cycleInterval = actionObject.actionInterval;
 
-		if(!available){
-			const interval = setInterval(() => {
-				if(actionObject.type === 'Collect') {
-					timeSwitch(actionObject.type)
-				} else {
-					if(actionObject.itemRequirements && checkInventoryRequirements(actionObject.itemRequirements)) {
-						timeSwitch(actionObject.type)
-					} else {
-						setWork(false)
-						setAvailable(true)
-						setTime(0)
+				//each second update the state time and check if the time is at an interval to call doActivity
+				const interval = setInterval(() => {
+
+					if(actionObject.type === 'Collect') {
+						timeSwitch(actionObject.type, cycleTime, cycleInterval)
+					}else{
+
+						if(actionObject.itemRequirements && checkInventoryRequirements(actionObject.itemRequirements)) {
+							timeSwitch(actionObject.type, cycleTime, cycleInterval)
+						}else{
+							setWork(false)
+							setAvailable(true)
+							setTime(0)
+						}
 					}
-				}
-			}, 125);
+			}, 1000);
+
 			return () => clearInterval(interval);
 		}
+	}else{
+		setTime(0);
+		setActionTime(1);
+	}
+
 
 	// eslint-disable-next-line
     }, [time, work, props.id, doActivity, setWork, available, action, checkInventoryRequirements, getObject])
@@ -96,24 +134,34 @@ const Activity = (props) => {
 	//activate the interval inside useEffect() by setting available to false
 	const startActivity = (e) => {
 
+		//set the current action by id
 		setAction(e.target.id)
 
-        if(!work) {
+		//get the action objects
+		let actionObject = getObject(e.target.id, actions_json["actions"]);
+
+		//get the action objects cycleSeconds
+		let actionCycleSeconds = actionObject.cycleSeconds;
+
+		//[check if the players associated skill level allows for loop iteration of actions]
+		let actionLoopIterations = 1;
+
+		let actionTotalTime = actionCycleSeconds * actionLoopIterations;
+
+		setActionTime(actionTotalTime);
+
+		//if the action is currently iterating
+		if(!work) {
 			setAvailable(false)
 			setWork(true)
-			setTime(divisor)
-			//doActivity(props.id, e.target.id)
+			setTime(0)
         }
-
-
     }
 
 	const cancelActivity = () => {
-
 		setAvailable(true)
 		setWork(false)
 		setTime(0)
-
 	}
 
 	return <div style={container_styles}>
@@ -131,7 +179,7 @@ const Activity = (props) => {
 			<p style={{margin: '25px 0 0 10px', fontSize: 18}}>{props.name}</p>
 		</div>
 
-		<ProgressBar width={time} />
+		<ProgressBar width={(time / actionTime) * 100} />
 
 		{/* Button Container */}
 		<div style={{display: 'flex', width: '100%', justifyContent: 'space-evenly'}}>
