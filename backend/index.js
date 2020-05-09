985003391c0a97ab9c9ceb1ff116678609903f33
therjmurray <therjmurray@gmail.com>
2020-05-09 13:44:02 -0400

    setting up

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+require('dotenv').config()
+
+const app = express();
+const port = 5000
+
+app.use(cors());
+app.use(express.json())
+
+// Connection URL
+let password = encodeURI(process.env.DB_PASSWORD)
+const url = 'mongodb+srv://admin:' + password + '@cluster0-0pgmo.mongodb.net/test?retryWrites=true&w=majority';
+
+// Create a new MongoClient
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => console.log('connected to db'))
+
+const usersRouter = require('./routes/users')
+app.use('/users', usersRouter)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
